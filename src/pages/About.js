24c7bc0a8a85f593bee1a8373d2abf76c44be3a9
Yury Layeuski <yury@residenetwork.com>
2021-03-17 10:40:18 -0700

    initil commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Bla bla bla about us Bla bla bla about us Bla bla bla about us
+        Bla bla bla about us Bla bla bla about us Bla bla bla about us
+        Bla bla bla about us Bla bla bla about us Bla bla bla about us
+        Bla bla bla about us Bla bla bla about us Bla bla bla about us
+        Bla bla bla about us Bla bla bla about us Bla bla bla about us
+        Bla bla bla about us Bla bla bla about us Bla bla bla about us
+      </p>
+    </section>
+  )
+}
+
+export default About
